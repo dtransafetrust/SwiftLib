7dfbdad224d81f288a7fd6fc43a2684b9db3feb4
Danh Tran <dtran@safetrust.com>
2022-07-01 17:15:42 +0700

    Initial project setup

---
+++ SwiftyLibExamples/SwiftyLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExamplesApp.swift
+//  SwiftyLibExamples
+//
+//  Created by safetrust on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
