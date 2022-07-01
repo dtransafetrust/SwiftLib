b3f7d2976723b000315f6cd6c4c46785d718df6d
Danh Tran <dtran@safetrust.com>
2022-07-01 18:03:14 +0700

    Added test cases

---
+++ SwiftLib/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by safetrust on 01/07/2022.
+//
+
+import Foundation
+
+public final class SwiftLib {
+    
+    let name = "SwiftLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
