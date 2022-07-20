3e4201dac97fa6ffcd3e82e2236174bb7cbb27a9
Danh Tran <dtran@safetrust.com>
2022-07-20 19:59:36 +0700

    Update release for v0.0.1

---
+++ ReleaseNotes.md
@@ -0,0 +1,11 @@
+# Release Notes
+
+## [0.0.2]
+
+- Support instance alive when app is not running.
+- Other bug fixes.
+
+## [0.0.1]
+
+- Latest XCode and Swift 5.4 support
+- Fix Lint building error for Android.
---
+++ ReleaseOldTag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright (c) Safetrust, Inc. - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR
+
+# ./ReleaseWithTag.sh v0.0.2 2c29f59
+./ReleaseWithTag.sh v0.0.1 27cbaf8
