76ee8b67f7df14b7e1710850c093fa36c2c10bfb
Danh Tran <dtran@safetrust.com>
2022-07-20 17:37:40 +0700

    Update release note

--- running.sh
+++ running.sh
@@ -11,21 +11,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-# git add .
+git add .
 
-# git commit -m "Fix bug"
+git commit -m "Fix bug"
 
-# git tag -fa $VERSION -m "Update $VERSION"
+git tag -fa $VERSION -m "Update $VERSION"
 
-# git push origin :$VERSION
+git push origin :$VERSION
 
-# git push origin $VERSION
-
-if GIT_DIR=/path/to/repo/.git git rev-parse $VERSION >/dev/null 2>&1
-then
-    echo "Found tag"
-else
-    echo "Tag not found"
-fi
+git push origin $VERSION
 
 # git push origin master
